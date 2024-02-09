9210d1231ae3a03a0b10a2643169a1e7668076fa
andrea-livornese <andrealivornese@gmail.com>
2024-02-09 16:16:53 +0100

    creazione funzione che calcola il range di tempo tra una data all'altra

--- js/script.js
+++ js/script.js
@@ -21,3 +21,96 @@ Successivamente bisogna addizionare al risultato di prima l'orario scelto nella 
 così conosciamo il tempo in giorni ore minuti e secondi che mancano.
 
 */
+
+
+
+// variabili che rappresentano il giorno l'ora minuti e secondi di quando deve terminare il timer
+
+let gg = 12;
+let hours= 9;
+let minutes = 30;
+let seconds = 0;
+
+
+// prendiamo la data corrente
+const data = new Date();
+
+// prendiamo il numero del giorno corrente
+let currentGg = 9; 
+// data.getDate();
+// prendiamo l'ora corrente
+let currentHour = 16; 
+// data.getHours();
+// prendiamo i minuti correnti
+let currentMinute = 0;  
+// data.getMinutes();
+// prendiamo i secondi correnti
+let currentSeconds =0; 
+// data.getSeconds();
+
+
+let timerGg = 0;
+
+let timerHour = 0;
+
+let timerMinute = 0;
+
+let timerSeconds = 0;
+
+
+
+dateSubtraction();
+
+console.log(timerGg, timerHour, timerMinute, timerSeconds);
+
+
+
+
+
+
+function dateSubtraction(){
+
+    if(seconds >= currentSeconds){
+        timerSeconds = seconds - currentSeconds;
+    }else{
+
+        seconds += 60;
+
+        minutes--;
+
+        timerSeconds = seconds - currentSeconds;
+    }
+
+    if(minutes >= currentMinute){
+
+        timerMinute = minutes - currentMinute;
+
+    }else{
+
+        minutes += 60;
+
+        hours--;
+
+        timerMinute = seconds - currentMinute;
+    }
+
+    if(hours >= currentHour){
+
+        timerHour = hours - currentHour;
+
+    }else{
+
+        hours += 24;
+
+        gg--;
+
+        timerHour = hours - currentHour;
+    }
+
+    if(gg >= currentGg){
+
+        timerGg = gg - currentGg;
+
+    }
+
+}
